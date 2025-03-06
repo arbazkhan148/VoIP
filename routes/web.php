e0879b95fabcdb80d8d6872cea71b8f65dc155f4
arbazkhan148 <khan.arbaz148@gmail.com>
2025-03-06 12:13:17 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,12 @@ Route::prefix('user')->name('user')->group(function(){
 
 Route::prefix('seller')->name('seller')->group(function(){
     Route::middleware(['guest:web'])->group(function () {
-    });
-    Route::middleware(['auth:seller'])->group(function () {
         Route::get('dashboard', [SellerController::class, 'dashboard'])->name('dashboard');
         Route::get('profile', [SellerController::class, 'profile'])->name('profile');
         Route::get('register', [SellerController::class, 'register'])->name('register');
         Route::get('contact', [SellerController::class, 'contact'])->name('contact');
+    });
+    Route::middleware(['auth:seller'])->group(function () {
     });
 });
 
