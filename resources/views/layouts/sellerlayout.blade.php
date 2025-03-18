4e6daee4faeecbad3cf2501c23f199848100c900
arbazkhan148 <khan.arbaz148@gmail.com>
2025-03-18 11:21:35 +0530

    Update sellerlayout.blade.php

--- resources/views/layouts/sellerlayout.blade.php
+++ resources/views/layouts/sellerlayout.blade.php
@@ -34,18 +34,18 @@
 
 <body>
 
-<!-- ======= Header ======= -->
-<header id="header" class="header fixed-top d-flex align-items-center">
+    <!-- ======= Header ======= -->
+    <header id="header" class="header fixed-top d-flex align-items-center">
 
-    <div class="d-flex align-items-center justify-content-between">
-        <a href="{{route('seller.logout')}}" class="logo d-flex align-items-center">
-            <img src="assets/img/logo.png" alt="">
-            <span class="d-none d-lg-block">VM Softech</span>
-        </a>
-        <i class="bi bi-list toggle-sidebar-btn"></i>
-    </div><!-- End Logo -->
+        <div class="d-flex align-items-center justify-content-between">
+            <a href="{{ route('seller.logout') }}" class="logo d-flex align-items-center">
+                <img src="assets/img/logo.png" alt="">
+                <span class="d-none d-lg-block">VM Softech</span>
+            </a>
+            <i class="bi bi-list toggle-sidebar-btn"></i>
+        </div><!-- End Logo -->
 
-    {{-- <div class="search-bar">
+        {{-- <div class="search-bar">
         <form class="search-form d-flex align-items-center" method="POST" action="#">
             <input type="text" name="query" placeholder="Search" title="Enter search keyword">
             <button type="submit" title="Search"><i class="bi bi-search"></i></button>
@@ -220,7 +220,7 @@
                         </li>
 
                         <li>
-                            <a class="dropdown-item d-flex align-items-center" href="{{route('seller.profile')}}">
+                            <a class="dropdown-item d-flex align-items-center" href="{{ route('seller.profile') }}">
                                 <i class="bi bi-person"></i>
                                 <span>My Profile</span>
                             </a>
@@ -237,7 +237,7 @@
                         </li>
 
                         <li>
-                            <a class="dropdown-item d-flex align-items-center" href="{{route('seller.logout')}}">
+                            <a class="dropdown-item d-flex align-items-center" href="{{ route('seller.logout') }}">
                                 <i class="bi bi-box-arrow-right"></i>
                                 <span>Sign Out</span>
                             </a>
@@ -249,78 +249,82 @@
             </ul>
         </nav><!-- End Icons Navigation -->
 
-</header><!-- End Header -->
+    </header><!-- End Header -->
 
-<!-- ======= Sidebar ======= -->
-<aside id="sidebar" class="sidebar">
+    <!-- ======= Sidebar ======= -->
+    <aside id="sidebar" class="sidebar">
 
-    <ul class="sidebar-nav" id="sidebar-nav">
+        <ul class="sidebar-nav" id="sidebar-nav">
 
-        <li class="nav-item">
-            <a class="nav-link {{request()->is('seller/dashboard') ? 'active' : '' }}"
-               href="{{ url('seller/dashboard') }}">
-                <i class="bi bi-grid"></i>
-                <span>Dashboard</span>
-            </a>
-        </li><!-- End Dashboard Nav -->
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('seller/dashboard') ? 'active' : '' }}"
+                    href="{{ url('seller/dashboard') }}">
+                    <i class="bi bi-grid"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li><!-- End Dashboard Nav -->
 
-        <li class="nav-item">
-            <a class="nav-link {{request()->is('seller/distributor/*')? 'active' : '' }} collapsed" href="#" data-bs-target="#distributor-nav" data-bs-toggle="collapse">
-                <i class="bi bi-person"></i>
-                <span>Distributor</span>
-                <i class="bi bi-chevron-down ms-auto"></i>
-            </a>
-            <ul id="distributor-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav" style="">
-                <li>
-                    <a href="{{route('seller.distributoradd')}}">
-                        <i class="bi bi-circle"></i><span>Add</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="{{route('seller.distributorlist')}}">
-                        <i class="bi bi-circle"></i><span>List</span>
-                    </a>
-                </li>
-            </ul>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link {{request()->is('seller/consumer/*')? 'active' : '' }} collapsed" href="#" data-bs-target="#consumer-nav" data-bs-toggle="collapse">
-                <i class="bi bi-person"></i>
-                <span>Consumer</span>
-                <i class="bi bi-chevron-down ms-auto"></i>
-            </a>
-            <ul id="consumer-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav" style="">
-                <li>
-                    <a href="{{route('seller.consumeradd')}}">
-                        <i class="bi bi-circle"></i><span>Add</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="{{route('seller.consumerlist')}}">
-                        <i class="bi bi-circle"></i><span>List</span>
-                    </a>
-                </li>
-            </ul>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link {{request()->is('seller/profile')? 'active' : '' }}" href="{{url('seller/profile')}}">
-                <i class="bi bi-person"></i>
-                <span>Profile</span>
-            </a>
-        </li><!-- End Profile Page Nav -->
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('seller/distributor/*') ? 'active' : '' }} collapsed" href="#"
+                    data-bs-target="#distributor-nav" data-bs-toggle="collapse">
+                    <i class="bi bi-person"></i>
+                    <span>Distributor</span>
+                    <i class="bi bi-chevron-down ms-auto"></i>
+                </a>
+                <ul id="distributor-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav" style="">
+                    <li>
+                        <a href="{{ route('seller.distributoradd') }}">
+                            <i class="bi bi-circle"></i><span>Add</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('seller.distributorlist') }}">
+                            <i class="bi bi-circle"></i><span>List</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('seller/consumer/*') ? 'active' : '' }} collapsed" href="#"
+                    data-bs-target="#consumer-nav" data-bs-toggle="collapse">
+                    <i class="bi bi-person"></i>
+                    <span>Consumer</span>
+                    <i class="bi bi-chevron-down ms-auto"></i>
+                </a>
+                <ul id="consumer-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav" style="">
+                    <li>
+                        <a href="{{ route('seller.consumeradd') }}">
+                            <i class="bi bi-circle"></i><span>Add</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('seller.consumerlist') }}">
+                            <i class="bi bi-circle"></i><span>List</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('seller/profile') ? 'active' : '' }}"
+                    href="{{ url('seller/profile') }}">
+                    <i class="bi bi-person"></i>
+                    <span>Profile</span>
+                </a>
+            </li><!-- End Profile Page Nav -->
 
-        <li class="nav-item">
-            <a class="nav-link {{request()->is('seller/contact')? 'active' : '' }}" href="{{url('seller/contact')}}">
-                <i class="bi bi-envelope"></i>
-                <span>Contact</span>
-            </a>
-        </li><!-- End Contact Page Nav -->
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('seller/contact') ? 'active' : '' }}"
+                    href="{{ url('seller/contact') }}">
+                    <i class="bi bi-envelope"></i>
+                    <span>Contact</span>
+                </a>
+            </li><!-- End Contact Page Nav -->
 
-    </ul>
+        </ul>
 
-</aside><!-- End Sidebar-->
+    </aside><!-- End Sidebar-->
 
-@yield('content')
+    @yield('content')
 
     <!-- ======= Footer ======= -->
     <footer id="footer" class="footer">
@@ -342,7 +346,7 @@
 
     <!-- Template Main JS File -->
     <script src="{{ asset('assets/js/main.js') }}"></script>
-@yield('page-js')
+    @yield('page-js')
 </body>
 
 </html>
