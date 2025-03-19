7ffeba86b99bd73a63e09c026fe5360c767453e2
arbazkhan148 <khan.arbaz148@gmail.com>
2025-03-19 17:11:48 +0530

    Update contact.blade.php

--- resources/views/distributor/contact.blade.php
+++ resources/views/distributor/contact.blade.php
@@ -21,30 +21,23 @@
                     <div class="row">
                         <div class="col-lg-6">
                             <div class="info-box card">
-                                <i class="bi bi-geo-alt"></i>
-                                <h3>Address</h3>
-                                <p>A108 Adam Street,<br>New York, NY 535022</p>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="info-box card">
                                 <i class="bi bi-telephone"></i>
                                 <h3>Call Us</h3>
-                                <p>+1 5589 55488 55<br>+1 6678 254445 41</p>
+                                <p>+971509036610</p>
                             </div>
                         </div>
                         <div class="col-lg-6">
                             <div class="info-box card">
                                 <i class="bi bi-envelope"></i>
                                 <h3>Email Us</h3>
-                                <p>info@example.com<br>contact@example.com</p>
+                                <p>info@citypharma.com</p>
                             </div>
                         </div>
                         <div class="col-lg-6">
                             <div class="info-box card">
-                                <i class="bi bi-clock"></i>
-                                <h3>Open Hours</h3>
-                                <p>Monday - Friday<br>9:00AM - 05:00PM</p>
+                                <i class="bi bi-geo-alt"></i>
+                                <h3>Address</h3>
+                                <p> Shaikh Zayed Road<br>Dubai</p>
                             </div>
                         </div>
                     </div>
