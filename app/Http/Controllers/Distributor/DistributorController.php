85fdfae58f21ee8d1b009e7374794c17244910ec
arbazkhan148 <khan.arbaz148@gmail.com>
2025-03-19 13:24:32 +0530

    Update DistributorController.php

--- app/Http/Controllers/Distributor/DistributorController.php
+++ app/Http/Controllers/Distributor/DistributorController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers\Distributor;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
+use Session;
+use DB;
+use Mail;
 
 class DistributorController extends Controller
 {
@@ -25,5 +30,23 @@ class DistributorController extends Controller
 
     public function plans(){
         return view('distributor.plans');
+    }
+
+    public function registerPOST(Request $request){
+        // dd($request->all());
+        User::create([
+            'first_name'=>$request->first_name,
+            'last_name'=>$request->last_name,
+            'phone'=>$request->phone,
+            'email'=>$request->email,
+            'password'=>$request->password,
+            'plan_type'=>$request->plan_type,
+            'plan_desc'=>$request->plan_desc,
+            'custom_input'=>$request->custom_input,
+        ]);
+        return redirect()->back()->with('success', 'Consumer Registered Successfully');
+    }
+
+    public function contactPOST(Request $request){
     }
 }
