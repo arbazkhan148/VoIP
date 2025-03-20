cc6029c316fe1c0d602f936f783f03cac9ccd1c5
arbazkhan148 <khan.arbaz148@gmail.com>
2025-03-20 12:12:56 +0530

    Update profile.blade.php

--- resources/views/distributor/profile.blade.php
+++ resources/views/distributor/profile.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.distributorlayout')
+
 @section('content')
     <main id="main" class="main">
 
@@ -6,7 +7,7 @@
             <h1>Profile</h1>
             <nav>
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item">Dashboard</a></li>
+                    <li class="breadcrumb-item">Dashboard</li>
                     <li class="breadcrumb-item active">Profile</li>
                 </ol>
             </nav>
@@ -14,270 +15,113 @@
 
         <section class="section profile">
             <div class="row">
-                <div class="col-xl-4">
-
-                    <div class="card">
-                        <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
-
-                            <img src="{{ asset('assets/img/profile-img.jpg') }}" alt="Profile" class="rounded-circle">
-                            <h2>Kevin Anderson</h2>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="col-xl-8">
-
+                <!-- Profile Details and Edit -->
+                <div class="col-xl-12">
                     <div class="card">
                         <div class="card-body pt-3">
-                            <!-- Bordered Tabs -->
+                            <!-- Tabs -->
                             <ul class="nav nav-tabs nav-tabs-bordered">
-
                                 <li class="nav-item">
                                     <button class="nav-link active" data-bs-toggle="tab"
                                         data-bs-target="#profile-overview">Overview</button>
                                 </li>
-
                                 <li class="nav-item">
                                     <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-edit">Edit
                                         Profile</button>
                                 </li>
-
                                 <li class="nav-item">
                                     <button class="nav-link" data-bs-toggle="tab"
                                         data-bs-target="#profile-change-password">Change Password</button>
                                 </li>
-
                             </ul>
+
                             <div class="tab-content pt-2">
-
+                                <!-- Overview Tab -->
                                 <div class="tab-pane fade show active profile-overview" id="profile-overview">
-
                                     <h5 class="card-title">Profile Details</h5>
 
                                     <div class="row">
-                                        <div class="col-lg-3 col-md-4 label ">First Name</div>
-                                        <div class="col-lg-9 col-md-8">Kevin</div>
+                                        <div class="col-lg-3 col-md-4 label">First Name</div>
+                                        <div class="col-lg-9 col-md-8">{{ $user->first_name }}</div>
                                     </div>
 
                                     <div class="row">
-                                        <div class="col-lg-3 col-md-4 label ">Last Name</div>
-                                        <div class="col-lg-9 col-md-8">Anderson</div>
+                                        <div class="col-lg-3 col-md-4 label">Last Name</div>
+                                        <div class="col-lg-9 col-md-8">{{ $user->last_name }}</div>
                                     </div>
 
                                     <div class="row">
                                         <div class="col-lg-3 col-md-4 label">Phone</div>
-                                        <div class="col-lg-9 col-md-8">(436) 486-3538 x29071</div>
+                                        <div class="col-lg-9 col-md-8">{{ $user->phone }}</div>
                                     </div>
 
                                     <div class="row">
                                         <div class="col-lg-3 col-md-4 label">Email</div>
-                                        <div class="col-lg-9 col-md-8">k.anderson@example.com</div>
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="col-lg-3 col-md-4 label">State</div>
-                                        <div class="col-lg-9 col-md-8">Jharkhand</div>
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="col-lg-3 col-md-4 label">City</div>
-                                        <div class="col-lg-9 col-md-8">Ranchi</div>
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="col-lg-3 col-md-4 label">Pin</div>
-                                        <div class="col-lg-9 col-md-8">835217</div>
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="col-lg-3 col-md-4 label">Address</div>
-                                        <div class="col-lg-9 col-md-8">A108 Adam Street, New York, NY 535022</div>
+                                        <div class="col-lg-9 col-md-8">{{ $user->email }}</div>
                                     </div>
 
                                 </div>
 
+                                <!-- Edit Profile Tab -->
                                 <div class="tab-pane fade profile-edit pt-3" id="profile-edit">
 
-                                    <!-- Profile Edit Form -->
-                                    <form>
-                                        <div class="row mb-3">
-                                            <label for="profileImage" class="col-md-4 col-lg-3 col-form-label">Profile
-                                                Image</label>
-                                            <div class="col-md-8 col-lg-9">
-                                                <img src="{{ asset('assets/img/profile-img.jpg') }}" alt="Profile">
-                                                <div class="pt-2">
-                                                    <a href="#" class="btn btn-primary btn-sm"
-                                                        title="Upload new profile image"><i class="bi bi-upload"></i></a>
-                                                    <a href="#" class="btn btn-danger btn-sm"
-                                                        title="Remove my profile image"><i class="bi bi-trash"></i></a>
-                                                </div>
-                                            </div>
+                                    @if (session('success'))
+                                        <div class="alert alert-success">
+                                            {{ session('success') }}
                                         </div>
+                                    @endif
+
+                                    <form action="{{ url('distributor/profile/update') }}" method="POST">
+                                        @csrf
+                                        @method('PUT')
 
                                         <div class="row mb-3">
-                                            <label for="firstName" class="col-md-4 col-lg-3 col-form-label">First Name</label>
+                                            <label for="firstName" class="col-md-4 col-lg-3 col-form-label">First
+                                                Name</label>
                                             <div class="col-md-8 col-lg-9">
-                                                <input name="firstName" type="text" class="form-control" id="firstName"
-                                                    value="Kevin">
+                                                <input name="first_name" type="text" class="form-control" id="firstName"
+                                                    value="{{ old('first_name', $user->first_name) }}">
                                             </div>
                                         </div>
 
                                         <div class="row mb-3">
                                             <label for="lastName" class="col-md-4 col-lg-3 col-form-label">Last Name</label>
                                             <div class="col-md-8 col-lg-9">
-                                                <input name="lastName" type="text" class="form-control" id="lastName"
-                                                    value="Anderson">
+                                                <input name="last_name" type="text" class="form-control" id="lastName"
+                                                    value="{{ old('last_name', $user->last_name) }}">
                                             </div>
                                         </div>
 
                                         <div class="row mb-3">
-                                            <label for="Phone" class="col-md-4 col-lg-3 col-form-label">Phone</label>
+                                            <label for="phone" class="col-md-4 col-lg-3 col-form-label">Phone</label>
                                             <div class="col-md-8 col-lg-9">
-                                                <input name="phone" type="text" class="form-control" id="Phone"
-                                                    value="(436) 486-3538 x29071">
+                                                <input name="phone" type="text" class="form-control" id="phone"
+                                                    value="{{ old('phone', $user->phone) }}">
                                             </div>
                                         </div>
 
                                         <div class="row mb-3">
-                                            <label for="Email" class="col-md-4 col-lg-3 col-form-label">Email</label>
+                                            <label for="email" class="col-md-4 col-lg-3 col-form-label">Email</label>
                                             <div class="col-md-8 col-lg-9">
-                                                <input name="email" type="email" class="form-control" id="Email"
-                                                    value="k.anderson@example.com">
-                                            </div>
-                                        </div>
-
-                                        <div class="row mb-3">
-                                            <label for="State" class="col-md-4 col-lg-3 col-form-label">State</label>
-                                            <div class="col-md-8 col-lg-9">
-                                                <input name="state" type="text" class="form-control" id="state"
-                                                    value="USA">
-                                            </div>
-                                        </div>
-
-                                        <div class="row mb-3">
-                                            <label for="City" class="col-md-4 col-lg-3 col-form-label">City</label>
-                                            <div class="col-md-8 col-lg-9">
-                                                <input name="city" type="text" class="form-control" id="city"
-                                                    value="Ranchi">
-                                            </div>
-                                        </div>
-
-                                        <div class="row mb-3">
-                                            <label for="Pin" class="col-md-4 col-lg-3 col-form-label">Pin</label>
-                                            <div class="col-md-8 col-lg-9">
-                                                <input name="pin" type="text" class="form-control" id="pin"
-                                                    value="835217">
-                                            </div>
-                                        </div>
-
-                                        <div class="row mb-3">
-                                            <label for="Address" class="col-md-4 col-lg-3 col-form-label">Address</label>
-                                            <div class="col-md-8 col-lg-9">
-                                                <input name="address" type="text" class="form-control" id="Address"
-                                                    value="A108 Adam Street, New York, NY 535022">
+                                                <input name="email" type="email" class="form-control" id="email"
+                                                    value="{{ old('email', $user->email) }}">
                                             </div>
                                         </div>
 
                                         <div class="text-center">
                                             <button type="submit" class="btn btn-primary">Save Changes</button>
                                         </div>
-                                    </form><!-- End Profile Edit Form -->
-
+                                    </form>
                                 </div>
 
-                                <div class="tab-pane fade pt-3" id="profile-settings">
+                                <!-- Change Password Tab (keep as is or modify as per your logic) -->
 
-                                    <!-- Settings Form -->
-                                    <form>
-
-                                        <div class="row mb-3">
-                                            <label for="fullName" class="col-md-4 col-lg-3 col-form-label">Email
-                                                Notifications</label>
-                                            <div class="col-md-8 col-lg-9">
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="changesMade"
-                                                        checked>
-                                                    <label class="form-check-label" for="changesMade">
-                                                        Changes made to your account
-                                                    </label>
-                                                </div>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="newProducts"
-                                                        checked>
-                                                    <label class="form-check-label" for="newProducts">
-                                                        Information on new products and services
-                                                    </label>
-                                                </div>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="proOffers">
-                                                    <label class="form-check-label" for="proOffers">
-                                                        Marketing and promo offers
-                                                    </label>
-                                                </div>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="securityNotify"
-                                                        checked disabled>
-                                                    <label class="form-check-label" for="securityNotify">
-                                                        Security alerts
-                                                    </label>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="text-center">
-                                            <button type="submit" class="btn btn-primary">Save Changes</button>
-                                        </div>
-                                    </form><!-- End settings Form -->
-
-                                </div>
-
-                                <div class="tab-pane fade pt-3" id="profile-change-password">
-                                    <!-- Change Password Form -->
-                                    <form>
-
-                                        <div class="row mb-3">
-                                            <label for="currentPassword" class="col-md-4 col-lg-3 col-form-label">Current
-                                                Password</label>
-                                            <div class="col-md-8 col-lg-9">
-                                                <input name="password" type="password" class="form-control"
-                                                    id="currentPassword">
-                                            </div>
-                                        </div>
-
-                                        <div class="row mb-3">
-                                            <label for="newPassword" class="col-md-4 col-lg-3 col-form-label">New
-                                                Password</label>
-                                            <div class="col-md-8 col-lg-9">
-                                                <input name="newpassword" type="password" class="form-control"
-                                                    id="newPassword">
-                                            </div>
-                                        </div>
-
-                                        <div class="row mb-3">
-                                            <label for="renewPassword" class="col-md-4 col-lg-3 col-form-label">Re-enter
-                                                New Password</label>
-                                            <div class="col-md-8 col-lg-9">
-                                                <input name="renewpassword" type="password" class="form-control"
-                                                    id="renewPassword">
-                                            </div>
-                                        </div>
-
-                                        <div class="text-center">
-                                            <button type="submit" class="btn btn-primary">Change Password</button>
-                                        </div>
-                                    </form><!-- End Change Password Form -->
-
-                                </div>
-
-                            </div><!-- End Bordered Tabs -->
-
+                            </div><!-- End tab content -->
                         </div>
                     </div>
-
                 </div>
             </div>
         </section>
 
-    </main><!-- End #main -->
+    </main>
 @endsection
